98f0a4d195f57580fc5b515d60e7327fab37d59d
shahadalyaqub <shahadalyaqub@gmail.com>
2024-10-19 20:37:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+{
+     int a = 10;
+     int b = 5;
+
+        int Addition = a+b ;
+        int Subtraction = a-b ;
+        int Multiplication = a*b;
+        int Division = a/b ;
+
+           Console.WriteLine("Addition:" + Addition);
+           Console.WriteLine("Subtraction:" + Subtraction);
+           Console.WriteLine("Multiplication:" + Multiplication);
+           Console.WriteLine("Division:" + Division);
+
+} 
